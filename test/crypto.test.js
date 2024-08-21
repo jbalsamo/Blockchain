ce3eb0829561fcf1f7cf6f630c6a5e8be74dbb71
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2024-08-21 11:39:21 -0400

    test still fails but code is working

--- test/crypto.test.js
+++ test/crypto.test.js
@@ -2,15 +2,15 @@ import { describe, expect, test } from "bun:test";
 import { decryptData, encryptData } from "../src/aescrypto.js";
 
 describe("aescrypto", () => {
-  test("should encrypt and decrypt data", () => {
+  test("should encrypt and decrypt data", async () => {
     const data = {
       patientId: "12345",
       diagnosis: "Common Cold",
       medication: "Aspirin",
     };
     const secretKey = "ThisIsAVerySecretKey";
-    const encryptedData = encryptData(data, secretKey);
-    const decryptedData = decryptData(encryptedData, secretKey);
+    const encryptedData = await encryptData(data, secretKey);
+    const decryptedData = await decryptData(encryptedData, secretKey);
     expect(decryptedData).toEqual(data);
   });
 });
